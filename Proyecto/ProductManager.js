3525743344f1fcad83eb86a19d5b2f7f77fcab2b
Santiago <santikessler7@gmail.com>
2023-12-19 22:59:34 -0300

    mi segundo commit

--- Proyecto/ProductManager.js
+++ Proyecto/ProductManager.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+
 class ProductManager {
   static #products = [];
+  static #dataFilePath = 'products.json';
 
   id;
   title;
@@ -7,6 +10,9 @@ class ProductManager {
   price;
   stock;
 
+  constructor() {
+    this.loadDataFromFile();
+  }
 
   create(data) {
     const requiredProps = ["title", "photo", "price", "stock"];
@@ -24,26 +30,37 @@ class ProductManager {
       const id = this.#generateProductId();
       const newProduct = { id, ...data };
       ProductManager.#products.push(newProduct);
+      this.saveDataToFile();
     }
   }
 
- 
   read() {
     return ProductManager.#products;
   }
 
-  
- 
-  
   #generateProductId() {
     return (
       ProductManager.#products[ProductManager.#products.length - 1]?.id + 1 || 1
     );
   }
+
+  loadDataFromFile() {
+    try {
+      const data = fs.readFileSync(ProductManager.#dataFilePath, 'utf8');
+      ProductManager.#products = JSON.parse(data);
+    } catch (error) {
+  
+      ProductManager.#products = [];
+    }
+  }
+
+  saveDataToFile() {
+    fs.writeFileSync(ProductManager.#dataFilePath, JSON.stringify(ProductManager.#products), 'utf8');
+  }
 }
 
-
 const prodManager = new ProductManager();
+
 
 prodManager.create({
   title: "Iphone11",
@@ -52,7 +69,9 @@ prodManager.create({
   stock: 25,
 });
 
+
 console.log(prodManager.read());
+
 
 prodManager.create({
   title: "Iphone 13",
@@ -64,7 +83,9 @@ prodManager.create({
 prodManager.create({
   title: "Iphone 15",
   photo: "https://images.unsplash.com/photo-1695048132783-4b9f77bde5be?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  price: 1.000,
+  price: 1000,
   stock: 15,
 });
+
+
 console.log(prodManager.read());
--- Proyecto/UserManager.js
+++ Proyecto/UserManager.js
@@ -1,49 +1,70 @@
+const fs = require('fs');
+
 class UserManager {
-    static #usersData = [];
-  
-    id;
-    name;
-    avatar;
-    email;
-  
-    addUser(data) {
-      const requiredProps = ["name", "avatar", "email"];
-      const providedProps = Object.keys(data);
-  
-      const missingProps = requiredProps.filter(prop => !providedProps.includes(prop));
-  
-      if (missingProps.length) {
-        console.log(`Missing properties: ${missingProps.join(" ")}`);
-      } else {
-        const id = UserManager.#usersData[UserManager.#usersData.length - 1]?.id + 1 || 1;
-  
-        UserManager.#usersData.push({ id, ...data });
-      }
-    }
-  
-    getUsers() {
-      return UserManager.#usersData;
-    }
-  
-    getUserById(id) {
-      return UserManager.#usersData.find(user => user.id == id);
+  static #usersData = [];
+  static #dataFilePath = 'users.json';
+
+  id;
+  name;
+  avatar;
+  email;
+
+  constructor() {
+    this.loadDataFromFile();
+  }
+
+  addUser(data) {
+    const requiredProps = ["name", "avatar", "email"];
+    const providedProps = Object.keys(data);
+
+    const missingProps = requiredProps.filter(prop => !providedProps.includes(prop));
+
+    if (missingProps.length) {
+      console.log(`Propiedades faltantes: ${missingProps.join(" ")}`);
+    } else {
+      const id = UserManager.#usersData[UserManager.#usersData.length - 1]?.id + 1 || 1;
+      UserManager.#usersData.push({ id, ...data });
+      this.saveDataToFile();
     }
   }
-  
-  const userManager = new UserManager();
-  
-  userManager.addUser({
-    name: "Lionel Messi",
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3hwkJNq2UGSmLooMiirndrTIXsW3UTIbIXaZjcLMjtmbWJY1IZVWIWN3boAR884u9Vq0&usqp=CAU",
-    email: "Lionelmessi@gmail.com",
-  });
-  
-  userManager.addUser({
-    name: " Paulo dybala",
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThvRTdOBhjNlFDyGqeBOkiDtdKCRZVa5J0pA&usqp=CAU",
-    email: "Lajoya@gmail.com",
-  });
-  
-  console.log(userManager.getUsers());
-  console.log(userManager.getUserById(2));
-  
+
+  getUsers() {
+    return UserManager.#usersData;
+  }
+
+  getUserById(id) {
+    return UserManager.#usersData.find(user => user.id == id);
+  }
+
+  loadDataFromFile() {
+    try {
+      const data = fs.readFileSync(UserManager.#dataFilePath, 'utf8');
+      UserManager.#usersData = JSON.parse(data);
+    } catch (error) {
+      
+      UserManager.#usersData = [];
+    }
+  }
+
+  saveDataToFile() {
+    fs.writeFileSync(UserManager.#dataFilePath, JSON.stringify(UserManager.#usersData), 'utf8');
+  }
+}
+
+const userManager = new UserManager();
+
+userManager.addUser({
+  name: "Lionel Messi",
+  avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3hwkJNq2UGSmLooMiirndrTIXsW3UTIbIXaZjcLMjtmbWJY1IZVWIWN3boAR884u9Vq0&usqp=CAU",
+  email: "Lionelmessi@gmail.com",
+});
+
+userManager.addUser({
+  name: "Paulo Dybala",
+  avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThvRTdOBhjNlFDyGqeBOkiDtdKCRZVa5J0pA&usqp=CAU",
+  email: "Lajoya@gmail.com",
+});
+
+console.log(userManager.getUsers());
+console.log(userManager.getUserById(2));
+
